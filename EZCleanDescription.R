108163408fb2a1b70b9c96674d9b73e60e37d3e6
AnthonyGalluzzo <145059259+AnthonyGalluzzo@users.noreply.github.com>
2024-11-03 18:58:06 -0500

    Add files via upload

---
+++ EZCleanDescription.R
@@ -0,0 +1,11 @@
+"Package: EZClean
+Title: It simplifies the data cleaning process, making it more accessible and efficient for data scientists and analysts
+Version: 0.1
+Authors@R: Anthony Galluzzo <asg289@usf.edu> [aut, cre]
+Description: EZClean is an R package designed to simplify the data cleaning process, 
+making it more accessible and efficient for data analysts and scientists. With a suite of intuitive functions, 
+EZClean helps users prepare their datasets for analysis by addressing common issues such as missing values, 
+duplicate entries, inconsistent formatting, and outlier detection.
+Depends: R (>= 4.4.1)
+License: CC0
+LazyData: true"
